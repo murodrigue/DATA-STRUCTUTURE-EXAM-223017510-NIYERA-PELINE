2aec570afe3a90c4b950468d16df8ef0b61e43cc
murodrigue <musonirodriguez@gmail.com>
2025-02-21 14:52:54 +0200

    Update example2.cpp

--- example2.cpp
+++ example2.cpp
@@ -3,4 +3,11 @@ using namespace std;
 int main() {
  int age = 20;
  double price = 19.99; 
- char grade = '
+ char grade = 'A';
+STRING NAME= "ALICE";
+COUT<<"NAME:"<<NAME<<ENDL;
+COUT<<"AGE:"<<AGE<<ENDL;
+COUT<<"PRICE:"<<"PRICE"<<PRICE<<ENDL;
+COUT<<"GRADE:"<<GRADE<<ENDL;
+RETURN 0;
+}
